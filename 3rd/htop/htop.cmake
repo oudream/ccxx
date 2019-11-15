ec025230f6bbbb87c183998fd4b5b957e31e9581
oudream <oudream@126.com>
2019-11-15 17:10:24 +0800

    Update htop.cmake

--- 3rd/htop/htop.cmake
+++ 3rd/htop/htop.cmake
@@ -9,6 +9,7 @@ set(gs_htop_include_path ${gs_htop_path})
 macro(mc_htop_generate_config)
     file(GLOB htop_config_h "${gs_htop_path}/htop/config.h")
 
+    message('htop config.h--: ' "${gs_htop_path}/htop/config.h")
     if (NOT htop_config_h)
         execute_process(
                 COMMAND ./autogen.sh
@@ -16,7 +17,7 @@ macro(mc_htop_generate_config)
                 RESULT_VARIABLE result
                 WORKING_DIRECTORY "${gs_htop_path}/htop"
         )
-        message('htop config.h--: ' ${result})
+        message('htop config.h result--: ' ${result})
     endif ()
 endmacro()
 
