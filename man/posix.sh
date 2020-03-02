e5acc7d70d8615323821f21b7b04e590b90faf55
oudream <oudream@126.com>
2020-03-02 17:12:01 +0800

    Create posix.sh

---
+++ man/posix.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+open http://www.open-std.org/JTC1/SC22/WG15/
+open http://www.open-std.org/jtc1/sc22/open/n4217.pdf
+open http://www.open-std.org/JTC1/SC22/WG15/docs/options.htm
+open https://i.linuxtoy.org/docs/guide/ch48s05.html
+
+open "/opt/ddd/os/posix/Portable Operating System Interface (POSIX) Operating System Interface.pdf"
