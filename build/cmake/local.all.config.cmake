318ac9446a483ecc31fff3e04ea60a9bf8a4ff4b
oudream <oudream@126.com>
2020-03-02 12:10:35 +0800

    add local.config.cmake type

--- build/cmake/local.all.config.cmake
+++ build/cmake/local.all.config.cmake
@@ -10,47 +10,9 @@
 ### sudo yum install unixODBC-devel
 
 
-###setting cmake option:
-##-DQT_QMAKE_EXECUTABLE:FILEPATH=F:/qt5.5.1mingw4.9/5.5/mingw492_32/bin/qmake.exe
-##-DCMAKE_PREFIX_PATH:FILEPATH=F:/qt5.5.1mingw4.9/5.5/mingw492_32
-
-# Find the QtWidgets library
-if(APPLE)
-    list(APPEND CMAKE_PREFIX_PATH /Users/oudream/Qt5.5.1/5.5/clang_64)
-    set(gs_prefix_path_python /Library/Frameworks/Python.framework/Versions/3.6)
-elseif(UNIX)
-    list(APPEND CMAKE_PREFIX_PATH /fff/qt5.6.3/5.6.3/gcc_64)
-elseif(WIN32)
-    message("CMAKE_SIZEOF_VOID_P:" ${CMAKE_SIZEOF_VOID_P})
-    if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
-        set(gs_prefix_path_python C:/Python/Python36-64)
-    else()
-        set(gs_prefix_path_python C:/Python/Python36-32)
-    endif()
-    if(MSVC)
-        message(${MSVC_VERSION})
-        if(MSVC_VERSION EQUAL 1800) # 1800 is Visual Studio 2013
-            list(APPEND CMAKE_PREFIX_PATH F:/qt5.6.3/5.6.3/msvc2013)
-        elseif(MSVC_VERSION EQUAL 1900)
-            list(APPEND CMAKE_PREFIX_PATH F:/qt5.6.1msvc2015/5.6/msvc2015)
-        elseif(MSVC_VERSION GREATER 1910)
-            list(APPEND CMAKE_PREFIX_PATH F:/qt5.12.3/5.12.3/msvc2017)
-#            list(APPEND CMAKE_PREFIX_PATH F:/qt5.10.1/5.10.1/msvc2017_64)
-        else()
-            message("can not find qt version!!!")
-        endif()
-    else()
-#        list(APPEND CMAKE_PREFIX_PATH F:/qt5.5.1mingw4.9.2x86/5.5/mingw492_32)
-#        list(APPEND CMAKE_PREFIX_PATH F:/qt5.5.1mingw4.9/5.5/mingw492_32)
-    endif()
-endif()
-
+### * setting cmake option:
 set(CMAKE_CXX_STANDARD 11)
 set(BUILD_SHARED_LIBS ON)
-
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DDEBUG")
-set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -DDEBUG")
-
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
 if(APPLE)
@@ -59,18 +21,61 @@ if(APPLE)
     set(CMAKE_MACOSX_RPATH 0)
 endif()
 
-### qt
-### if enable qt, then config "build/ccpro/local.config.cmake" 's CMAKE_PREFIX_PATH to your dir of qt
+if (CMAKE_BUILD_TYPE STREQUAL Release)
+    SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${gs_project_output_path_release})
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DRELEASE")
+    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -DRELEASE")
+else()
+    SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${gs_project_output_path_debug})
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DDEBUG")
+    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -DDEBUG")
+endif()
+message("CMAKE_RUNTIME_OUTPUT_DIRECTORY--: " ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
+
+###  * qt
+### if enable qt, then config "build/ccpro/local.all.config.cmake" 's CMAKE_PREFIX_PATH to your dir of qt
 set(gs_project_enable_qt 0)
 find_package(Qt5Core)
 if (Qt5Core_LIBRARIES)
     set(gs_project_enable_qt 1)
+
+    ##-DQT_QMAKE_EXECUTABLE:FILEPATH=F:/qt5.5.1mingw4.9/5.5/mingw492_32/bin/qmake.exe
+    ##-DCMAKE_PREFIX_PATH:FILEPATH=F:/qt5.5.1mingw4.9/5.5/mingw492_32
+    # Find the QtWidgets library
+    if(APPLE)
+        list(APPEND CMAKE_PREFIX_PATH /Users/oudream/Qt5.5.1/5.5/clang_64)
+        set(gs_prefix_path_python /Library/Frameworks/Python.framework/Versions/3.6)
+    elseif(UNIX)
+        list(APPEND CMAKE_PREFIX_PATH /fff/qt5.6.3/5.6.3/gcc_64)
+    elseif(WIN32)
+        message("CMAKE_SIZEOF_VOID_P:" ${CMAKE_SIZEOF_VOID_P})
+        if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+            set(gs_prefix_path_python C:/Python/Python36-64)
+        else()
+            set(gs_prefix_path_python C:/Python/Python36-32)
+        endif()
+        if(MSVC)
+            message(${MSVC_VERSION})
+            if(MSVC_VERSION EQUAL 1800) # 1800 is Visual Studio 2013
+                list(APPEND CMAKE_PREFIX_PATH F:/qt5.6.3/5.6.3/msvc2013)
+            elseif(MSVC_VERSION EQUAL 1900)
+                list(APPEND CMAKE_PREFIX_PATH F:/qt5.6.1msvc2015/5.6/msvc2015)
+            elseif(MSVC_VERSION GREATER 1910)
+                list(APPEND CMAKE_PREFIX_PATH F:/qt5.12.3/5.12.3/msvc2017)
+            else()
+                message("can not find qt version!!!")
+            endif()
+        else()
+            list(APPEND CMAKE_PREFIX_PATH F:/qt5.5.1mingw4.9/5.5/mingw492_32)
+        endif()
+    endif()
 endif ()
 #set(gs_project_enable_qt 1)
 # set(gs_project_qt_version 4)
 
 
-### cpython
+### * cpython
 set(gs_project_enable_cpython 0)
 find_package(PythonLibs)
 if (PYTHON_LIBRARIES)
@@ -80,12 +85,12 @@ endif ()
 # set(gs_project_cpython_version 2)
 
 
-### libuv
+### * libuv
 set(gs_project_enable_uv 1)
 #set(gs_project_enable_uv 0)
 
 
-### curl
+### * curl
 set(gs_project_enable_curl 0)
 if (NOT WIN32)
     find_package(CURL)
@@ -98,7 +103,7 @@ endif ()
 #set(gs_project_enable_curl 1)
 
 
-### openssl
+### * openssl
 set(gs_project_enable_openssl 1)
 if (NOT WIN32)
     find_package (OpenSSL)
@@ -111,7 +116,7 @@ endif ()
 #set(gs_project_enable_openssl 0)
 
 
-### opencv
+### * opencv
 set(gs_project_enable_opencv 1)
 #set(gs_project_enable_opencv 0)
 if (WIN32)
@@ -123,16 +128,16 @@ if (gs_project_enable_opencv)
 endif ()
 
 
-### yamlcpp yaml-cpp
+### * yamlcpp yaml-cpp
 set(gs_project_enable_yamlcpp 1)
 #set(gs_project_enable_yamlcpp 0)
 
-### yamlcpp yaml-cpp
+### * hiredis
 set(gs_project_enable_hiredis 1)
 #set(gs_project_enable_hiredis 0)
 
 
-### gtest googletest
+### * gtest googletest
 set(gtest_build_tests 1)
 set(gtest_build_samples 1)
 set(gmock_build_tests 1)
@@ -141,12 +146,12 @@ set(gtest_build_samples 0)
 set(gmock_build_tests 0)
 
 
-### benchmark
+### * benchmark
 set(BENCHMARK_ENABLE_TESTING ON)
 set(BENCHMARK_ENABLE_TESTING OFF)
 
 
-### htop
+### * htop
 set(gs_project_enable_htop 1)
 ### cd /opt/ddd/ccpp/ccxx/3rd/htop/readme.sh
 ### ./autogen.sh && ./configure --disable-unicode
@@ -184,4 +189,3 @@ endif()
 
 
 message("CMAKE_PREFIX_PATH:" ${CMAKE_PREFIX_PATH})
-
---
+++ build/cmake/local.simple.config.cmake
@@ -0,0 +1,33 @@
+
+### mac
+### brew install unixodbc
+
+### Ubuntu: -D"OTL_ODBC_UNIX" -lodbc
+### sudo apt-get install unixodbc unixodbc-dev
+### sudo apt-get install uuid-dev
+
+### CentOS: -D"OTL_ODBC_UNIX" -lodbc
+### sudo yum install unixODBC-devel
+
+
+### * setting cmake option:
+set(CMAKE_CXX_STANDARD 11)
+set(BUILD_SHARED_LIBS ON)
+set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+
+if(APPLE)
+    # http://kescoode.com/cmake-rpath-problem/
+    # https://stackoverflow.com/questions/31561309/cmake-warnings-under-os-x-macosx-rpath-is-not-specified-for-the-following-targe
+    set(CMAKE_MACOSX_RPATH 0)
+endif()
+
+if (CMAKE_BUILD_TYPE STREQUAL Release)
+    SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${gs_project_output_path_release})
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DRELEASE")
+    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -DRELEASE")
+else()
+    SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${gs_project_output_path_debug})
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DDEBUG")
+    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -DDEBUG")
+endif()
+message("CMAKE_RUNTIME_OUTPUT_DIRECTORY--: " ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
