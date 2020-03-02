a3be9142697b6de4f07ed71113720a4b181055b2
oudream <oudream@126.com>
2020-03-02 17:12:52 +0800

    Update 3rd.cmake

--- 3rd/3rd.cmake
+++ 3rd/3rd.cmake
@@ -1,15 +1,15 @@
 
-if (${gs_project_enable_uv})
+if (gs_project_enable_uv)
     include(${gs_project_path}/3rd/uv.cmake)
 endif ()
 
 
-if (${gs_project_enable_yamlcpp})
+if (gs_project_enable_yamlcpp)
     include(${gs_project_path}/3rd/yamlcpp.cmake)
 endif ()
 
 
-if (${gs_project_enable_hiredis})
+if (gs_project_enable_hiredis)
     include(${gs_project_path}/3rd/hiredis.cmake)
 endif ()
 
