8e54aa3f8c7a8b154a507f6ab00b260887dfe8b0
oudream <oudream@126.com>
2020-03-05 11:14:49 +0800

    c++ call python had moved to hello-cmake

--- test/python/setup-noddy-shoddy.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-setup(name="cxpy", version="1.0",
-      ext_modules=[
-          Extension("noddy", ["noddy/noddy_module.cpp","noddy/noddy_config.hpp","noddy/noddy1.cpp","noddy/noddy2.cpp","noddy/noddy3.cpp","noddy/noddy4.cpp"]),
-          Extension("shoddy", ["shoddy/shoddy_module.cpp","shoddy/shoddy_config.hpp","shoddy/shoddy1.cpp"]),
-      ])
--- test/python/shoddy/test-shoddy.py
+++
@@ -1,9 +0,0 @@
-import shoddy
-
-s = shoddy.Shoddy1(range(3))
-s.extend(s)
-print(len(s))
-
-print(s.increment())
-
-print(s.increment())
